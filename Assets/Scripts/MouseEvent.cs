3f620b09a3373553675233f7b6572cf9b1fc4413
Alvin Lee <nsmae0127@gmail.com>
2015-07-29 17:37:08 +0900

    Updated 150729

---
+++ Assets/Scripts/MouseEvent.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseEvent : MonoBehaviour
+{
+	public AudioSource audio;
+
+	void Start ()
+	{
+		audio = GetComponent<AudioSource> ();
+	}
+	
+	void OnMouseEnter ()
+	{
+		audio.Play ();
+	}
+}
+ 
